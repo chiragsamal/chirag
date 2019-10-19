5f59df94daed9a34ffc0c43624b8c6e5aaa03fb1
Chirag Samal <52205244+chiragsamal@users.noreply.github.com>
2019-10-19 16:12:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $(function(){
 	$("#about-me").typed({
-		strings: ["Student at IIIT-NR.","Co-ordinator at E-Cell IIIT-NAYA RAIPUR","ex-Sales and Marketing Executive at HostelDhaba","Design Team Member at Technovate","Open Source Enthusiast.","Interested in Machine Learning and AI.","Loves to Code <&#x2764>."],
+		strings: ["Student at IIIT-NR.","Co-ordinator at E-Cell IIIT-NAYA RAIPUR","Sales and Marketing Executive at HostelDhaba","Design Team Member at Technovate","Open Source Enthusiast.","Interested in Machine Learning and AI.","Loves to Code <&#x2764>."],
 		typeSpeed: 30,
 		loop: true,
 	});
